f31638f3e732959a341c75dbe7b23302889bffd8
satinderkaurjnv-dot <satinderkaurjnv@gmail.com>
2026-08-31 18:26:30 +0530

    Delete src directory

--- src/components/sections/hero-layers/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./CursorParallax";
-export * from "./MineralDustField";
-export * from "./SeismicSweep";
-export * from "./GlowPulse";
-export * from "./OrbitTooltips";
-export * from "./StarfieldBackground";
-export * from "./ConnectionArcs";
